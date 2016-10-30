0770770d0b8eaa081d64fe102bba92aeb77aa36a
jain <jain@jains-MacBook-Pro.local>
2016-10-30 18:38:06 -0400

    Modified test_bb_mp.py. Probably need to do some custom pickling and check on timing.

--- test_bb_mp.py
+++ test_bb_mp.py
@@ -9,12 +9,12 @@ def newton(x):
     '''
     xi = 1
     true_root = math.sqrt(x)
-    for i in range(0,1000):
+    for i in range(0,20):
         xi = .5*(xi+x/xi)
         blackbox.log('iter', i)
         blackbox.log('xi', xi)
         error(xi, true_root)
-        blackbox.save(verbose=False)
+        blackbox.save(verbose=True)
     return xi
 
 def error(x,y):
@@ -22,25 +22,23 @@ def error(x,y):
     compute the error
     '''
     err = abs(x-y)
-    #blackbox.log('error', err)
+    blackbox.log('error', err)
     return err
 
 def test_par_runs(i):
+    blackbox.takeoff(name='Newtons method worker:{}'.format(i), description='sample run', force=True)    
     r = newton(i)
+    blackbox.land()
     return r
 
-blackbox.set_experiment('SquareRootPar3')
-blackbox.takeoff(name='Newton method par %i'%(7), description='sample run', force=True)    
-#pool = Pool(4)
-#result  = pool.map(test_par_runs, [2, 3, 4, 5])
-#pool.close()
-print test_par_runs(8)
-blackbox.land()
+blackbox.set_experiment('SquareRootParallel')
+pool = Pool(4)
+result  = pool.map(test_par_runs, [2, 3, 4, 5])
+pool.close()
 
-
-# exp = blackbox.get_experiment('SquareRootPar3')
-# print exp.list_runs()
-# run = exp.get_run('Newton method par 7')
-# print run.events
+exp = blackbox.get_experiment('SquareRootParallel')
+print exp.list_runs()
+run = exp.get_run('Newtons method worker:2')
+print run.events
 
 
